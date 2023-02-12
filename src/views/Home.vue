aad2825a2d1f00f5c909db59ec006fe97d634064
lyr <liyingrui19981104@163.com>
2023-02-12 19:35:31 +0800

    创建Vue项目

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <a-button danger>Default</a-button>
   </div>
 </template>
 
@@ -11,8 +10,8 @@ import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default defineComponent({
   name: 'Home',
-  components: {
-    HelloWorld,
-  },
+  // components: {
+  //   HelloWorld,
+  // },
 });
 </script>
